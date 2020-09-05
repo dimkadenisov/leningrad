de0952f65d031aa81b9d63e4b3e6258e2b991014
1 <a1@MacBook-Air-1.local>
2020-09-05 21:15:18 +0300

    macbook air commit

---
+++ src/scripts/components/hideButton.js
@@ -0,0 +1,7 @@
+document.body.addEventListener('click', e => {
+	const button = e.currentTarget.closest('.hide-button');
+	if (button) {
+		if (button.classList.contain('hide-button_opened')) {
+		}
+	}
+});
--- src/scripts/components/linksSwiper.js
+++ src/scripts/components/linksSwiper.js
@@ -1,9 +1,25 @@
 const linksSwiper = new Swiper('.links-swiper__inner', {
-	slidesPerView: 5,
+	slidesPerView: 1,
 	spaceBetween: 18,
 	// loop: true,
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
+	},
+	breakpoints: {
+		// when window width is >= 320px
+		500: {
+			slidesPerView: 2,
+		},
+		700: {
+			slidesPerView: 3,
+		},
+		// when window width is >= 480px
+		1000: {
+			slidesPerView: 4,
+		},
+		1330: {
+			slidesPerView: 5,
+		},
 	},
 });
