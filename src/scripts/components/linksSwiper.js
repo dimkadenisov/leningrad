a60742f1e5cb025c07a51bb337493a9fa24f5901
Dima Denisov <2denisov@bk.ru>
2020-09-06 17:55:28 +0300

    increment

--- src/scripts/components/linksSwiper.js
+++ src/scripts/components/linksSwiper.js
@@ -1,7 +1,6 @@
 const linksSwiper = new Swiper('.links-swiper__inner', {
 	slidesPerView: 5,
 	spaceBetween: 18,
-	// loop: true,
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
