bf071485e151838250c3816cedbff6d766581866
dimkadenisov <2denisov@bk.ru>
2020-09-06 22:51:19 +0000

    Deploying to gh-pages from  @ 9f617e180e4873a8c590825ac17b75bf4784f274 🚀

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
 "use strict";
 
 document.body.addEventListener('click', e => {
-  const button = e.currentTarget.closest('.hide-button');
-
-  if (button) {
-    if (button.classList.contain('hide-button_opened')) {}
-  }
+  const button = e.target.closest('.hide-button');
+  if (!button) return;
+  button.closest('.container').querySelector('.hidden-block').classList.toggle('hidden-block_opened');
+  button.closest('.container').querySelectorAll('.hide-button').forEach(item => {
+    item.classList.toggle('hide-button_opened');
+  });
 });
 "use strict";
 
@@ -48,7 +49,7 @@ const linksSwiper = new Swiper('.links-swiper__inner', {
   },
   breakpoints: {
     // when window width is >= 320px
-    500: {
+    541: {
       slidesPerView: 2
     },
     700: {
