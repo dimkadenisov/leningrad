714b6d4984c03508c76f90d8ce42fe94cbdcb21b
dimkadenisov <2denisov@bk.ru>
2020-09-11 15:08:15 +0000

    Deploying to gh-pages from  @ d48d2ef6e421d7e87407e11916421b1570420bd2 🚀

--- scripts/script.js
+++ scripts/script.js
@@ -10,32 +10,15 @@ document.body.addEventListener('click', e => {
 });
 "use strict";
 
-const handleLanguageSelectorOpen = function (event) {
-  this.classList.add('language-selector_opened');
-};
-
-const handleLanguageSelectorCLose = function (event) {
-  this.classList.remove('language-selector_opened');
-};
-
-document.querySelectorAll('.language-selector').forEach(selector => {
-  selector.addEventListener('click', handleLanguageSelectorOpen, handleLanguageSelectorCLose);
+document.body.addEventListener('click', e => {
+  const selector = e.target.closest('.language-selector');
+  if (!selector) return;
+  selector.classList.toggle('language-selector_opened');
 });
-document.querySelectorAll('.language').forEach(language => {
-  language.addEventListener('click', function (event) {
-    if (language.closest('.language-list')) {
-      const appendTarget = language.parentNode;
-      const selector = language.closest('.language-selector');
-      const oldLanguage = selector.querySelector('.language-selector__current .language');
-      oldLanguage.parentNode.appendChild(language);
-      appendTarget.appendChild(oldLanguage);
-      selector.classList.remove('language-selector_opened');
-      selector.removeEventListener('click', handleLanguageSelectorOpen);
-      setTimeout(() => {
-        selector.addEventListener('click', handleLanguageSelectorOpen);
-      }, 200);
-    }
-  });
+document.body.addEventListener('click', e => {
+  const currentLanguage = e.target.closest('.language-selector__current > .language');
+  if (!currentLanguage) return;
+  e.preventDefault();
 });
 "use strict";
 
